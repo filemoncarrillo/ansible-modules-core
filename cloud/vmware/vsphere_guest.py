315350a2ed54ad7fbeda59ac5cfce6fe2266c21b
testfilemon <testfilemon@gmail.com>
2016-05-08 20:26:16 -0700

    revert to April 26 version

--- cloud/vmware/vsphere_guest.py
+++ cloud/vmware/vsphere_guest.py
@@ -203,7 +203,7 @@ EXAMPLES = '''
 
 # Reconfigure the CPU and Memory on the newly created VM
 # Will return the changes made
-# Note: reserved_cpu is in MHz 
+# Note: reserved_cpu is in MHz
 
 - vsphere_guest:
     vcenter_hostname: vcenter.mydomain.local
@@ -1550,7 +1550,7 @@ def main():
         'num_cpus': int,
         'scsi': basestring,
         'osid': basestring,
-        'reserved_memory': int,
+	'reserved_memory': int,
         'reserved_cpu': int
     }
 
